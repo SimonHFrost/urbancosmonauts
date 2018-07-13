f95a5a9c4ae8f92dc8878809bddc9404684a6399
simonhfrost <simonhfrost@gmail.com>
2018-07-13 14:57:14 +0200

    Use normalize.css

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.css"/>
     <link href="<?php echo get_bloginfo('template_directory'); ?>/blog.css" rel="stylesheet">
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/section.css" rel="stylesheet">
   </head>
   <body>
     <div class="container">
       <header>
         <img class="nav-image" src="http://radio.urbancosmonaut.com/wp-content/uploads/2015/08/logo_nav.png"/>
         <div class="nav-content">
-          <h1>Urban Cosmonaut Radio</h1>
+          <h1>URBAN COSMONAUT RADIO</h1>
           <nav>
             <ol>
               <li>Radio</li>
@@ -19,8 +21,14 @@
           </nav>
         </div>
       </header>
-      <h2>Radio</h2>
-      <h2>Releases</h2>
+      <div class="content">
+        <div class="section">
+          <h2>Radio</h2>
+        </div>
+        <div class="section">
+          <h2>Releases</h2>
+        </div>
+      </div>
     </div>
   </body>
 </html>
