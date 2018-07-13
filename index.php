028cb49dd1aa229c9c9db4e75f5cb4438f532ba6
simonhfrost <simonhfrost@gmail.com>
2018-07-13 14:29:28 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/blog.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <h1>Urban Cosmonaut Radio</h1>
+        <nav>
+          <ol>
+            <li>Radio</li>
+            <li>Releases</li>
+            <li>Contact</li>
+          </ol>
+        </nav>
+      </header>
+      <h2>Radio</h2>
+      <h2>Releases</h2>
+    </div>
+  </body>
+</html>
