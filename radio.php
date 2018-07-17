8ec02464d9898987f1ed5824b11aab7930830559
simonhfrost <simonhfrost@gmail.com>
2018-07-17 23:44:16 +0200

    correct category

--- radio.php
+++ radio.php
@@ -1,10 +1,10 @@
-<?php $my_query = new WP_Query( 'cat=4&posts_per_page=3' );
+<?php $my_query = new WP_Query( 'cat=5&posts_per_page=3' );
   while ( $my_query->have_posts() ) : $my_query->the_post();
       preg_match("/by (.+)/", get_the_title(), $artist_match_array);
       $my_artist = $artist_match_array[1];
 
-      preg_match("/UCR (#.+) by/", get_the_title(), $number_match_array);
-      $my_number = $number_match_array[1];
+      // preg_match("/UCR (#.+) by/", get_the_title(), $number_match_array);
+      // $my_number = $number_match_array[1];
 
       $my_date = get_the_date();
 ?>
@@ -12,20 +12,14 @@
     <div class="section-info background3">
       <h3><?php echo $my_artist ?></h3>
       <div class="section-details">
-        <h4 class="number"><?php echo $my_number ?></h4>
+        <!-- <h4 class="number"><?php echo $my_number ?></h4> -->
         <h4 class="type">Podcast</h4>
         <h4 class="date"><?php echo $my_date ?></h4>
 
         <?php
-          $featured_image = the_field('featured_image', 1109);
-          var_dump( $featured_image );
-          echo $featured_image;
+          $featured_image = the_field('featured_image');
 
           $size = 'full'; // (thumbnail, medium, large, full or custom size)
-
-          if( $featured_image ) {
-          	echo wp_get_attachment_image( $image, $size );
-          }
         ?>
       </div>
     </div>
