c231ad523d1c4bf46dfbd05dc97ce018cdd16820
simonhfrost <simonhfrost@gmail.com>
2018-07-18 22:40:56 +0200

    Display number again

--- radio.php
+++ radio.php
@@ -3,8 +3,8 @@
       preg_match("/by (.+)/", get_the_title(), $artist_match_array);
       $my_artist = $artist_match_array[1];
 
-      // preg_match("/UCR (#.+) by/", get_the_title(), $number_match_array);
-      // $my_number = $number_match_array[1];
+      preg_match("/Radio (#.+) by/", get_the_title(), $number_match_array);
+      $my_number = $number_match_array[1];
 
       $my_date = get_the_date();
       $featured_image = get_field('featured_image');
@@ -13,7 +13,7 @@
     <div class="section-info" style="background-image: url(<?php echo $featured_image['url']; ?>)";>
       <h3><?php echo $my_artist ?></h3>
       <div class="section-details">
-        <!-- <h4 class="number"><?php echo $my_number ?></h4> -->
+        <h4 class="number"><?php echo $my_number ?></h4>
         <h4 class="type">Podcast</h4>
         <h4 class="date"><?php echo $my_date ?></h4>
       </div>
