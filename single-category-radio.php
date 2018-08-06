4664324d8e008ef88b617b0da6c799683fbc0ef5
simonhfrost <simonhfrost@gmail.com>
2018-08-06 23:46:12 +0200

    Start displaying info for individual radio instance

--- single-category-radio.php
+++ single-category-radio.php
@@ -1,1 +1,24 @@
-<h1>single-category-radio</h1>
+<?php get_template_part( 'header' ); ?>
+<div class="content radio-page">
+
+  <!-- TODO Should share code between this and radio.page.php -->
+  <div class="section">
+    <h2>RADIO</h2>
+
+    <div class="highlight-row">
+      <div class="selected-track-image" style="background-image: url(http://localhost:8888/urbancosmonauts/wp-content/uploads/2018/06/1.jpg); filter: grayscale(0%) !important;"></div>
+      <div class="selected-track-info">
+        <h4 class="number">#45</h4>
+        <h4 class="type">Podcast</h4>
+        <h4 class="date">June 13, 2018</h4>
+        <h4 class="artist-header">ISLANDMAN</h4>
+        <?php echo get_field('soundcloud_url'); ?>
+      </div>
+    </div>
+
+    <?php $param_cat = 5; $param_posts_per_page = 36; ?>
+    <?php include( locate_template( 'section-infos/section-infos.php', false, false ) ); ?>
+  </div>
+
+</div>
+<?php get_template_part( 'footer' ); ?>
