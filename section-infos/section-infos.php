59f4adf79bcccb43ed54d6fc6e235c52b5f30619
simonhfrost <simonhfrost@gmail.com>
2018-08-07 23:47:24 +0200

    Get info straight from custom fields instead of regex complexity

--- section-infos/section-infos.php
+++ section-infos/section-infos.php
@@ -1,27 +1,18 @@
 <div class="section-infos">
   <?php $my_query = new WP_Query( "cat={$param_cat}&posts_per_page={$param_posts_per_page}" );
     while ( $my_query->have_posts() ) : $my_query->the_post();
-        preg_match("/by (.+)/", get_the_title(), $artist_match_array);
-        $my_artist = ($artist_match_array) ? $artist_match_array[1] : 'ERROR';
-
-        preg_match("/(Radio |UCR )(#.+) by/", get_the_title(), $number_match_array);
-        $my_number = ($number_match_array) ? $number_match_array[2] : 'ERROR';
-
-        $my_date = get_the_date();
-        $featured_image = get_field('featured_image');
-
         global $post;
         $post_slug=$post->post_name;
   ?>
 
       <div class="section-info-parent">
-        <h3 class="artist-header" style="color: <?php echo get_field('artist_header_color')?> "> <?php echo $my_artist ?></h3>
+        <h3 class="artist-header" style="color: <?php echo get_field('artist_header_color')?> "> <?php echo get_field('artist_name') ?></h3>
 
-        <a class="section-info" href="./radio/<?php echo $post_slug ?>" style="background-image: url(<?php echo $featured_image['url']; ?>)";>
+        <a class="section-info" href="./radio/<?php echo $post_slug ?>" style="background-image: url(<?php echo get_field('featured_image')['url']; ?>)";>
           <div class="section-details">
-            <h4 class="number"><?php echo $my_number ?></h4>
-            <h4 class="type"><?php echo ($param_cat === 5) ? "Podcast" : "Release"?></h4>
-            <h4 class="date"><?php echo $my_date ?></h4>
+            <h4 class="number"><?php echo get_field('title'); ?></h4>
+            <h4 class="type">Podcast</h4>
+            <h4 class="date"><?php echo get_the_date(); ?></h4>
           </div>
         </a>
 
