c6b41fee698e56ffe5a36be3747c7f417e645fe1
simonhfrost <simonhfrost@gmail.com>
2018-08-07 23:19:47 +0200

    Show actual date in feature row

--- radio-page/highlight-row.php
+++ radio-page/highlight-row.php
@@ -3,7 +3,7 @@
   <div class="selected-track-info">
     <h4 class="number">#45</h4>
     <h4 class="type">Podcast</h4>
-    <h4 class="date">June 13, 2018</h4>
+    <h4 class="date"><?php echo get_the_date(); ?></h4>
     <h4 class="artist-header">ISLANDMAN</h4>
     <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/<?php echo $track_number ?>&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
   </div>
