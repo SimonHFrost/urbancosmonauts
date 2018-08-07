28f2e712eccc01376e6164f847545efe3e3688bb
simonhfrost <simonhfrost@gmail.com>
2018-08-07 22:43:58 +0200

    Create highlight-row template function

---
+++ radio-page/highlight-row.php
@@ -0,0 +1,10 @@
+<div class="highlight-row">
+  <div class="selected-track-image" style="background-image: url(http://localhost:8888/urbancosmonauts/wp-content/uploads/2018/06/1.jpg); filter: grayscale(0%) !important;"></div>
+  <div class="selected-track-info">
+    <h4 class="number">#45</h4>
+    <h4 class="type">Podcast</h4>
+    <h4 class="date">June 13, 2018</h4>
+    <h4 class="artist-header">ISLANDMAN</h4>
+    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/<?php echo $track_number ?>&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+  </div>
+</div>
