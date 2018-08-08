99d9d1ff2fb6b177199059192a3985d9b1276f2b
simonhfrost <simonhfrost@gmail.com>
2018-08-08 20:14:49 +0200

    Move soundcloud player to left

--- radio-page/highlight-row.php
+++ radio-page/highlight-row.php
@@ -5,6 +5,6 @@
     <h4 class="type">Podcast</h4>
     <h4 class="date"><?php echo get_the_date(); ?></h4>
     <h4 class="artist-header" style="color: <?php echo get_field('artist_header_color') ?>"><?php echo get_field('artist_name') ?></h4>
-    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/<?php echo get_field('soundcloud_track_id') ?>&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+    <iframe class="soundcloud-player" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/<?php echo get_field('soundcloud_track_id') ?>&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
   </div>
 </div>
