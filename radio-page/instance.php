d5d390692992010697f0a2bd53617705bb4a0b1e
simonhfrost <simonhfrost@gmail.com>
2018-08-08 22:55:29 +0200

    Merge instance and index radio pages

--- radio-page/instance.php
+++ radio-page/instance.php
@@ -1,11 +1,28 @@
+<?php /* Template Name: radio-page/index.php */ ?>
+
 <?php get_template_part( 'header' ); ?>
+
 <div class="content radio-page">
 
   <div class="section">
     <h2>RADIO</h2>
 
+    <?php
+    if (is_page()) {
+      // Get the first post if loading the index page
+      $latest_post = get_posts("cat=5&numberposts=1");
+      $ID = $latest_post[0]->ID;
+
+      global $post;
+      $post = get_post( $ID, OBJECT );
+      setup_postdata( $post );
+    } else {
+      the_post();
+    }
+    ?>
+
     <?php include( locate_template( 'radio-page/highlight-row.php', false, false) ); ?>
-    <?php if (!$post) { the_post(); } ?>
+
     <?php echo the_content(); ?>
 
     <?php $param_cat = 5; $param_posts_per_page = 36; ?>
