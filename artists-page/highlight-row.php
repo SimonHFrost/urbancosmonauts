7041ecc6f6bf7829517ad49682e01ad460df598c
simonhfrost <simonhfrost@gmail.com>
2018-08-10 14:42:41 +0200

    color header on artist instance page

--- artists-page/highlight-row.php
+++ artists-page/highlight-row.php
@@ -1,3 +1,3 @@
+<h3 style="color: <?php echo get_field('artist_header_color')?>" ><?php echo get_the_title() ?></h4>
 <img style="background-size: contain; width: 200px; height: 200px; background-image: url(<?php echo get_field('featured_image')['url'] ?>);" />
-<h4><?php echo get_the_title() ?></h4>
 <p><?php echo the_content() ?></p>
