9ff20810559568f7adb875973e1df05884e0480f
simonhfrost <simonhfrost@gmail.com>
2018-08-10 14:51:58 +0200

    Add booking request button

--- artists-page/instance.php
+++ artists-page/instance.php
@@ -16,7 +16,6 @@
     <h3>Interested in booking an artist? Select the artist you want to book, and complete the form</h3>
 
     <div class="artist-section-infos">
-
       <?php $param_cat = 'artists'; $param_posts_per_page = 36; ?>
       <?php $my_query = new WP_Query( "category_name={$param_cat}&posts_per_page={$param_posts_per_page}" );
         while ( $my_query->have_posts() ) : $my_query->the_post();
@@ -29,9 +28,9 @@
             <a class="section-info" href="./radio/<?php echo $post_slug ?>" style="background-image: url(<?php echo get_field('featured_image')['url']; ?>)";></a>
           </div>
 
-    <?php endwhile;
-    wp_reset_postdata();
-    ?>
+      <?php endwhile;
+      wp_reset_postdata();
+      ?>
     </div>
 
 
