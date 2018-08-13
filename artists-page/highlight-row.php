caef3691c64bfe26e466df3cc750ed36a2554c3a
simonhfrost <simonhfrost@gmail.com>
2018-08-13 20:46:43 +0200

    style highlight-row for artists page

--- artists-page/highlight-row.php
+++ artists-page/highlight-row.php
@@ -1,4 +1,10 @@
-<h3 class="artist-header" style="color: <?php echo get_field('artist_header_color')?>" ><?php echo get_the_title() ?></h4>
-<img style="background-size: contain; width: 200px; height: 200px; background-image: url(<?php echo get_field('featured_image')['url'] ?>);" />
-<p><?php echo the_content() ?></p>
-<button>Booking request</button>
+<div class="artist-highlight-row">
+  <div>
+    <h3 class="artist-header" style="color: <?php echo get_field('artist_header_color')?>" ><?php echo get_the_title() ?></h4>
+    <img style="background-size: contain; width: 200px; height: 200px; background-image: url(<?php echo get_field('featured_image')['url'] ?>);" />
+  </div>
+  <div class="highlight-content">
+    <div class="summary"><?php echo the_content() ?></div>
+    <button>Booking request</button>
+  </div>
+</div>
--- header.php
+++ header.php
@@ -10,6 +10,7 @@
     <link href="<?php echo get_bloginfo('template_directory'); ?>/section-infos/section-infos.css" rel="stylesheet">
     <link href="<?php echo get_bloginfo('template_directory'); ?>/radio-page/radio-page.css" rel="stylesheet">
     <link href="<?php echo get_bloginfo('template_directory'); ?>/artists-page/artist-section-infos.css" rel="stylesheet">
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/artists-page/artist-highlight-row.css" rel="stylesheet">
   </head>
   <body>
     <div class="container">
