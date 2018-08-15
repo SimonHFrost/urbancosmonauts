e9915bcb38e274507e51d0da490933135360ba88
simonhfrost <simonhfrost@gmail.com>
2018-08-15 17:55:56 +0200

    Style headers

--- contact.php
+++ contact.php
@@ -5,6 +5,8 @@
 <div class="contact">
   <div class="content">
     <div class="contact-info">
+      <h2>Contact Us</h2>
+
       <h3>Urban Cosmonaut Radio</h3>
       <h3>Alexandrienstr. 118-121</h3>
       <h3>10969 Berlin</h3>
@@ -12,8 +14,13 @@
 
       <h3>+49 30 44 05 09 80</h3>
       <h3>info@urbancosmonaut.com</h3>
+
+      <h3>IG: @urbancosmonautradio</h3>
+      <h3>FB: /urbancosmonaut</h3>
     </div>
     <div class="contact-form">
+      <h2>Booking Form</h2>
+
       <?php
         $content = apply_filters('the_content', $post->post_content);
         echo $content;
