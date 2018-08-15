f0c336b59858e69ea985a3be52824f805b45990b
simonhfrost <simonhfrost@gmail.com>
2018-08-15 18:03:15 +0200

    Create contact-group class

--- contact.php
+++ contact.php
@@ -7,16 +7,22 @@
     <div class="contact-info">
       <h2>Contact Us</h2>
 
-      <h3>Urban Cosmonaut Radio</h3>
-      <h3>Alexandrienstr. 118-121</h3>
-      <h3>10969 Berlin</h3>
-      <h3>Germany</h3>
+      <div class="contact-group">
+        <h3>Urban Cosmonaut Radio</h3>
+        <h3>Alexandrienstr. 118-121</h3>
+        <h3>10969 Berlin</h3>
+        <h3>Germany</h3>
+      </div>
 
-      <h3>+49 30 44 05 09 80</h3>
-      <h3>info@urbancosmonaut.com</h3>
+      <div class="contact-group">
+        <h3>+49 30 44 05 09 80</h3>
+        <h3>info@urbancosmonaut.com</h3>
+      </div>
 
-      <h3>IG: @urbancosmonautradio</h3>
-      <h3>FB: /urbancosmonaut</h3>
+      <div class="contact-group">
+        <h3>IG: @urbancosmonautradio</h3>
+        <h3>FB: /urbancosmonaut</h3>
+      </div>
     </div>
     <div class="contact-form">
       <h2>Booking Form</h2>
