3f3327bfe4567bbb196202c9a39c7ffe6af613fd
simonhfrost <simonhfrost@gmail.com>
2018-08-15 17:53:03 +0200

    Create contact.css

--- contact.php
+++ contact.php
@@ -2,11 +2,24 @@
 
 <?php get_template_part( 'header' ); ?>
 
-<div class="content" style="color: white">
-  <?php
-    $content = apply_filters('the_content', $post->post_content);
-    echo $content;
-  ?>
+<div class="contact">
+  <div class="content">
+    <div class="contact-info">
+      <h3>Urban Cosmonaut Radio</h3>
+      <h3>Alexandrienstr. 118-121</h3>
+      <h3>10969 Berlin</h3>
+      <h3>Germany</h3>
+
+      <h3>+49 30 44 05 09 80</h3>
+      <h3>info@urbancosmonaut.com</h3>
+    </div>
+    <div class="contact-form">
+      <?php
+        $content = apply_filters('the_content', $post->post_content);
+        echo $content;
+      ?>
+    </div>
+  </div>
 </div>
 
 <?php get_template_part( 'footer' ); ?>
--- header.php
+++ header.php
@@ -11,6 +11,7 @@
     <link href="<?php echo get_bloginfo('template_directory'); ?>/radio-page/radio-page.css" rel="stylesheet">
     <link href="<?php echo get_bloginfo('template_directory'); ?>/artists-page/artist-section-infos.css" rel="stylesheet">
     <link href="<?php echo get_bloginfo('template_directory'); ?>/artists-page/artist-highlight-row.css" rel="stylesheet">
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/contact.css" rel="stylesheet">
   </head>
   <body>
     <div class="container">
