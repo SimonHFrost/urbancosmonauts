6e1909006c20af05a68895466bad65109e7f7086
simonhfrost <simonhfrost@gmail.com>
2018-09-14 14:59:46 +0200

    Created minimal header

---
+++ header-footer/minimal-header.php
@@ -0,0 +1,15 @@
+<header>
+  <div class="nav-content">
+    <nav>
+      <a class="nav-image-link" href="<?php echo get_bloginfo( 'wpurl' );?>">
+        <img class="nav-image-small" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.png"/>
+      </a>
+      <ul class="inline-nav">
+        <li><a class="radio-color-hover" href="./radio">Radio</a></li>
+        <li><a class="releases-color-hover" href="./releases">Releases</a></li>
+        <li><a class="artists-color-hover" href="./artists">Artists</a></li>
+        <li><a class="contact-color-hover" href="./contact">Contact Us</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
