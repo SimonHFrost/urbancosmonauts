1a7163319af32928c3e7e1e7c2c6f7706ea045a6
simonhfrost <simonhfrost@gmail.com>
2018-09-18 22:08:35 +0200

    More styling to artists page

--- artists-page/highlight-row.php
+++ artists-page/highlight-row.php
@@ -1,10 +1,10 @@
 <div class="artist-highlight-row">
   <div>
     <h3 class="artist-header" style="color: <?php echo get_field('artist_header_color')?>" ><?php echo get_the_title() ?></h4>
-    <img style="background-size: contain; width: 200px; height: 200px; background-image: url(<?php echo get_field('featured_image')['url'] ?>);" />
+    <img class="artist-image" style="background-image: url(<?php echo get_field('featured_image')['url'] ?>);" />
   </div>
   <div class="highlight-content">
-    <div class="summary"><?php echo the_content() ?></div>
+    <div class="summary" style="background-color: <?php echo get_field('artist_header_color')?>;"><?php echo the_content() ?></div>
     <a href="./contact?artists=<?php echo get_the_title()?>" class="book-button" style="color: <?php echo get_field('artist_header_color')?>; border-color:<?php echo get_field('artist_header_color')?>">Booking request</a>
   </div>
 </div>
