53f2868e593425b6504293c0c98a049ec12d964a
simonhfrost <simonhfrost@gmail.com>
2018-09-18 22:52:55 +0200

    Add social links to artist

--- artists-page/highlight-row.php
+++ artists-page/highlight-row.php
@@ -2,6 +2,8 @@
   <div class="highlight-info">
     <h3 class="artist-header" style="color: <?php echo get_field('artist_header_color')?>" ><?php echo get_the_title() ?></h3>
     <div class="artist-image" style="background-image: url(<?php echo get_field('featured_image')['url'] ?>);"></div>
+    <a href="<?php echo get_field('soundcloud_link') ?>"><img class="social-link" src="<?php echo get_bloginfo('template_directory'); ?>/images/soundcloud.png" /></a>
+    <a href="<?php echo get_field('facebook_link') ?>"><img class="social-link" src="<?php echo get_bloginfo('template_directory'); ?>/images/facebook.png" /></a>
   </div>
   <div class="highlight-content">
     <div class="summary" style="background-color: <?php echo get_field('artist_header_color')?>;"><?php echo the_content() ?></div>
