ddf0af9c9c9880a364257456305684d9df569a8c
simonhfrost <simonhfrost@gmail.com>
2018-10-10 22:47:16 +0200

    Style 404 page

--- 404.php
+++ 404.php
@@ -1,1 +1,10 @@
-<h1>404 Page Not Found</h1>
+<?php get_template_part( 'header-footer/head' ); ?>
+
+
+<div style="text-align: center; padding-top: 100px;">
+  <h1 style="color: white; font-size: 50px;">404 Page Not Found</h1>
+  <img src="<?php echo get_bloginfo('template_directory'); ?>/images/404.gif">
+</div>
+
+
+<?php get_template_part( 'header-footer/footer' ); ?>
