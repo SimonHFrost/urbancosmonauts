7f6e1b3113a5b6d878e61b357e7588637cf9a8c0
simonhfrost <simonhfrost@gmail.com>
2018-10-12 18:06:20 +0200

    Add support for set id on releases

--- releases-page/highlight-row.php
+++ releases-page/highlight-row.php
@@ -4,6 +4,16 @@
     <h4 class="title"><?php echo get_the_title(); ?></h4>
     <h4 class="date"><?php echo get_the_date(); ?></h4>
     <h4 class="artist-header" style="color: <?php echo get_field('artist_header_color') ?>"><?php echo get_field('artist_name') ?></h4>
-    <iframe class="soundcloud-player" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/<?php echo get_field('soundcloud_track_id') ?>&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+
+    <?php
+
+      if (get_field('soundcloud_track_id')) {
+        $SOUNDCLOUD_URL = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' . get_field('soundcloud_track_id') . '&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true';
+      } else {
+        $SOUNDCLOUD_URL = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/359391749' . get_field('soundcloud_set_id') . '&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true';
+      }
+
+    ?>
+    <iframe class="soundcloud-player" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="<?php echo $SOUNDCLOUD_URL ?>"></iframe>
   </div>
 </div>
