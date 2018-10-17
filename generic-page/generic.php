9926ee362c21558a7267f38667ad42e0a44219b6
simonhfrost <simonhfrost@gmail.com>
2018-10-17 23:05:18 +0200

    Add new headers for impressum page

---
+++ generic-page/generic.php
@@ -0,0 +1,14 @@
+<?php /* Template Name: contact-page/generic.php */ ?>
+
+<?php get_template_part( 'header-footer/head' ); ?>
+<?php get_template_part( 'header-footer/minimal-header' ); ?>
+
+
+<div class="content">
+  <?php
+    $content = apply_filters('the_content', $post->post_content);
+    echo $content;
+  ?>
+</div>
+
+<?php get_template_part( 'header-footer/footer' ); ?>
