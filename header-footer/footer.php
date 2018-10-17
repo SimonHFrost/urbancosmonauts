46ac244a1b2d878fb7d606c27ade8595917a0f93
simonhfrost <simonhfrost@gmail.com>
2018-10-17 23:15:10 +0200

    Add impressum link

--- header-footer/footer.php
+++ header-footer/footer.php
@@ -1,3 +1,6 @@
     </div>
+  <footer style="text-align: center; margin-bottom: 24px;">
+    <a href="./impressum" style="text-decoration: none; color: white; font-size: 24px;">Impressum</a>
+  </footer>
   </body>
 </html>
