e369b905e5058f7a652b6c9298256732dc585923
simonhfrost <simonhfrost@gmail.com>
2018-10-20 16:54:37 +0200

    Create link to privacy policy, extract css into css file

--- header-footer/footer.php
+++ header-footer/footer.php
@@ -1,6 +1,7 @@
     </div>
-  <footer style="text-align: right; margin: 24px auto; width: 1010px;">
-    <a href="./impressum" style="text-decoration: none; color: black; font-size: 18px;">Impressum</a>
+  <footer>
+    <a href="./impressum">Impressum</a>
+    <a href="./privacy-policy">Privacy Policy</a>
   </footer>
   <?php wp_footer(); ?>
   </body>
