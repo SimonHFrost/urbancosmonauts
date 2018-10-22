3ba7a60eae998486ede9cf99161e5c3c89a6d55e
simonhfrost <simonhfrost@gmail.com>
2018-10-22 16:01:59 +0200

    Update contact page to just have one field

--- contact-page/contact.php
+++ contact-page/contact.php
@@ -6,28 +6,8 @@
 
 <div class="contact">
   <div class="content">
-    <div class="contact-info">
-      <h2 class="contact-color">Contact</h2>
-
-      <div class="contact-group">
-        <h3>Urban Cosmonaut Radio</h3>
-        <h3>Alexandrienstr. 118-121</h3>
-        <h3>10969 Berlin</h3>
-        <h3>Germany</h3>
-      </div>
-
-      <div class="contact-group">
-        <h3>+49 30 44 05 09 80</h3>
-        <h3>booking@urbancosmonaut.com</h3>
-      </div>
-
-      <div class="contact-group">
-        <h3>IG: @urbancosmonautradio</h3>
-        <h3>FB: /urbancosmonaut</h3>
-      </div>
-    </div>
     <div class="contact-description">
-      <h2 class="contact-color">About Us</h2>
+
 
       <?php
         $content = apply_filters('the_content', $post->post_content);
