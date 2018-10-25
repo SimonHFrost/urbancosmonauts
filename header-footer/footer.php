fbc46f0808f0b55c341ccc6f7553e217a92603c2
simonhfrost <simonhfrost@gmail.com>
2018-10-25 22:30:04 +0200

    Make footer scroll with main content

--- header-footer/footer.php
+++ header-footer/footer.php
@@ -1,8 +1,8 @@
+      <footer>
+        <a href="./impressum">Impressum</a>
+        <a href="./privacy-policy">Privacy Policy</a>
+      </footer>
     </div>
-  <footer>
-    <a href="./impressum">Impressum</a>
-    <a href="./privacy-policy">Privacy Policy</a>
-  </footer>
   <?php wp_footer(); ?>
   </body>
 </html>
