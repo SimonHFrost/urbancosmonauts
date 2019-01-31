dd5028734f3516db2191f989396da8d4f6c62c18
simonhfrost <simonhfrost@gmail.com>
2019-01-31 10:25:01 +0100

    Add show more about this podcast again

--- radio-page/instance.php
+++ radio-page/instance.php
@@ -25,6 +25,15 @@
 
     <?php include( locate_template( 'radio-page/highlight-row.php', false, false) ); ?>
 
+    <?php if ($post->post_content) { ?>
+    <div class="page-content">
+      <details>
+        <summary>SEE MORE ABOUT THIS PODCAST</summary>
+        <?php echo the_content(); ?>
+      </details>
+    </div>
+    <?php } ?>
+
     <?php $param_cat = 'radio'; $param_posts_per_page = 100; ?>
     <?php include( locate_template( 'section-infos/section-infos.php', false, false ) ); ?>
   </div>
